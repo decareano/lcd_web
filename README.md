df940ff31cbe525e32fd755c0bfa37cc8b76b2a3
marcelo.gobelli@gmail.com <cloud_user@reydelcompas1c.mylabserver.com>
2019-09-20 17:24:35 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# lcd_web
+this is some text
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: lcd_web
+# Recipe:: default
+#
+# Copyright:: 2019, Student Name, All Rights Reserved.
